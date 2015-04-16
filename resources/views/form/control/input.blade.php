11d31680f233fd6f7da3e953a3231227b90c93b7
bjuppa <nilsved@gmail.com>
2015-04-16 10:22:31 +0200

    added aria-invalid

--- resources/views/form/control/input.blade.php
+++ resources/views/form/control/input.blade.php
@@ -1,12 +1,8 @@
-<input type="{{ $type or 'text' }}"
-       name="{{ $name }}"
-       class="form-control"
-       id="{{ $control_id }}"
-@unless(empty($control_description_id))
-       aria-describedby="{{ $control_description_id }}"
-@endif
-       value="{{ old($name, isset($value) ? $value : null) }}"
-@unless(empty($has_error))
-	aria-invalid="true"
-@endif
-		       >
+<input type="{{ $type or 'text' }}" name="{{ $name }}" class="form-control" id="{{ $control_id }}" value="{{ old($name, isset($value) ? $value : null) }}"
+	@unless(empty($control_description_id))
+    aria-describedby="{{ $control_description_id }}"
+  @endif
+  @unless(empty($has_error))
+    aria-invalid="true"
+  @endif
+>
