b272dbc6630cdbd6696754a2c3065efa9846f8ab
bjuppa <nilsved@gmail.com>
2015-04-16 16:30:26 +0200

    throws exception when name not set

--- src/ViewComposers/InputComposer.php
+++ src/ViewComposers/InputComposer.php
@@ -4,32 +4,35 @@ namespace FewAgency\TwbsBlade\ViewComposers;
 
 use Illuminate\Contracts\View\View;
 
-class InputComposer {
+class InputComposer
+{
 
-	/**
-	 * Bind data to the view.
-	 *
-	 * @param  View $view
-	 *
-	 * @return void
-	 */
-	public function compose(View $view)
-	{
-		//TODO: throw error if $view->name not set
-		$data['control_id'] = $view->bsb_pkg_ref . $view->form_id . $view->name; //TODO: handle unique id if name is repeated within the form (with arrays?)
-		if ( $view->errors->has($view->name) )  //TODO: make this pull out a named form's errors
-		{
-			$data['has_error'] = true;
-			//TODO: if errors are to be displayed at start of form, collect this into that section instead
-			$view->nest('error_content', $view->bsb_pkg_ref . '::form.control.errors',
-					[ 'errors' => $view->errors->get($view->name) ]);
-		}
-		if ( $view->error_content or $view->help_text or $view->help_content )
-		{
-			$data['control_description_id'] = $data['control_id'] . 'desc';
-		}
+    /**
+     * Bind data to the view.
+     *
+     * @param  View $view
+     *
+     * @return void
+     */
+    public function compose(View $view)
+    {
+        if (empty($view->name) and !str_contains($view->getName(), ['csrf', 'group'])) {
+            throw new \Exception('Input need a name parameter set in ' . $view->getPath());
+        }
+        $data['control_id'] = $view->bsb_pkg_ref . $view->form_id . $view->name; //TODO: handle unique id if name is repeated within the form (with arrays?)
+        if ($view->errors->has($view->name))  //TODO: also pull out a named form's errors
+        {
+            $data['has_error'] = true;
+            $errors_view = $view->bsb_pkg_ref . '::form.control.errors';
+            //TODO: if errors are to be displayed at start of form, collect this into that section instead
+            $view->nest('error_content', $errors_view, ['errors' => $view->errors->get($view->name)]);
+        }
 
-		$view->with($data);
-	}
+        if ($view->error_content or $view->help_text or $view->help_content) {
+            $data['control_description_id'] = $data['control_id'] . 'desc';
+        }
+
+        $view->with($data);
+    }
 
 }
