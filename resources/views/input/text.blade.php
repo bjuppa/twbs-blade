bcb43eb6365f803c6c9f0805e59645ccfaae83e1
bjuppa <nilsved@gmail.com>
2015-04-16 10:23:51 +0200

    added default type text to input

--- resources/views/input/text.blade.php
+++ resources/views/input/text.blade.php
@@ -1,5 +1,5 @@
 @extends($bsb_pkg_ref.'::form.group')
 
 @section($bsb_pkg_ref.'control_or_group')
-	@include($bsb_pkg_ref.'::form.control.input', ['type' => 'text'])
+	@include($bsb_pkg_ref.'::form.control.input', ['type' => empty($type) ? 'text' : $type])
 @overwrite
