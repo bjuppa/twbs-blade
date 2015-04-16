6a5a8c93054b3f60b186e7d21678e81313193f5d
bjuppa <nilsved@gmail.com>
2015-04-16 15:59:51 +0200

    not printing label if no string set

--- resources/views/form/control/label.blade.php
+++ resources/views/form/control/label.blade.php
@@ -1,4 +1,6 @@
-<label class="control-label" for="{{ $control_id }}">
-	{{-- TODO: The label should have .sr-only option --}}
-	{{ $label or $name }}
-</label>
+@unless(empty($label) and empty($email))
+	<label class="control-label" for="{{ $control_id }}">
+		{{-- TODO: The label should have .sr-only option --}}
+		{{ $label or $name }}
+	</label>
+@endunless
