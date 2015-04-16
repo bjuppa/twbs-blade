436d83719bfe4e69ee750daa2c05d5e68b730155
bjuppa <nilsved@gmail.com>
2015-04-16 10:21:36 +0200

    switched if isset to unless empty

--- resources/views/form/control/description.blade.php
+++ resources/views/form/control/description.blade.php
@@ -1,9 +1,9 @@
 <div id="{{ $control_description_id }}">
 	{!! $error_content or '' !!}
-	@if(isset($help_text))
-		@include($bsb_pkg_ref.'::form.help-block', ['content' => e($help_text), 'tag' => isset($help_block_tag) ? $help_block_tag : 'span'])
+	@unless(empty($help_text))
+		@include($bsb_pkg_ref.'::form.help-block', ['content' => e($help_text), 'tag' => empty($help_block_tag) ? 'span' : $help_block_tag])
 	@endif
-	@if(isset($help_content))
-		@include($bsb_pkg_ref.'::form.help-block', ['content' => $help_content, 'tag' => isset($help_block_tag) ? $help_block_tag : 'div'])
+	@unless(empty($help_content))
+		@include($bsb_pkg_ref.'::form.help-block', ['content' => $help_content, 'tag' => empty($help_block_tag) ? 'div' : $help_block_tag])
 	@endif
 </div>
