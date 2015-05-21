9e8f3dd43ba1931bbd79e4e036202ed2d0ee421d
bjuppa <nilsved@gmail.com>
2015-05-21 17:49:33 +0200

    if-statement around errors

--- resources/views/form/control/errors.blade.php
+++ resources/views/form/control/errors.blade.php
@@ -1,5 +1,7 @@
+@if(count($errors))
 <ul class="help-block list-unstyled">
-	@foreach($errors as $message)
-		<li>{{ $message }}</li>
-	@endforeach
+    @foreach($errors as $message)
+        <li>{{ $message }}</li>
+    @endforeach
 </ul>
+@endif
