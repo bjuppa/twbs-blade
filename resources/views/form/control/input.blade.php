32f7db3540ade08891f23c1aa73855d6efb059e9
bjuppa <nilsved@gmail.com>
2015-05-22 17:38:55 +0200

    using html-builder

--- resources/views/form/control/input.blade.php
+++ resources/views/form/control/input.blade.php
@@ -1,11 +1,23 @@
-<input type="{{ $type or 'text' }}" name="{{ $name }}" class="form-control" id="{{ $control_id }}"
-	@unless($type == 'password')
-		value="{{ old($name, isset($value) ? $value : null) }}"
-	@endunless
-	@unless(empty($control_description_id))
-    aria-describedby="{{ $control_description_id }}"
-  @endunless
-  @unless(empty($has_error))
-    aria-invalid="true"
-  @endunless
->
+<?php
+if (empty($input_attributes)) {
+    $input_attributes = [];
+}
+if (empty($input_attributes['type'])) {
+    $input_attributes['type'] = 'text';
+}
+if (!empty($type)) {
+    $input_attributes['type'] = $type;
+}
+$default_attributes = ['class' => 'form-control', 'value' => old($name)];
+if (!empty($control_description_id)) {
+    $default_attributes['aria-describedby'] = $control_description_id;
+}
+if (!empty($has_error)) {
+    $default_attributes['aria-invalid'] = 'true';
+}
+$force_attributes = ['id' => $control_id, 'name' => $name];
+if ($input_attributes['type'] == 'password') {
+    $force_attributes['value'] = null;
+}
+?>
+{!! bsb_html('input', $input_attributes, null, $default_attributes, $force_attributes) !!}
