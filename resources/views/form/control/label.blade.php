913cf2e685e5be4116fa9a3709d0b156c556a151
bjuppa <nilsved@gmail.com>
2015-06-03 10:31:53 +0200

    added html_label option to standard labels

--- resources/views/form/control/label.blade.php
+++ resources/views/form/control/label.blade.php
@@ -1,6 +1,10 @@
-@unless(empty($label) and empty($name))
-	<label class="control-label" for="{{ $control_id }}">
-		{{-- TODO: The label should have .sr-only option --}}
-		{{ $label or $name }}
-	</label>
+@unless(empty($label) and empty($name) and empty($html_label))
+    <label class="control-label" for="{{ $control_id }}">
+        {{-- TODO: The label should have .sr-only option --}}
+        @if(isset($html_label))
+            {!! $html_label !!}
+        @else
+            {{ $label or $name }}
+        @endif
+    </label>
 @endunless
