4993615ca0407d6622a3fc633c8424245f98baf6
bjuppa <nilsved@gmail.com>
2015-08-03 15:38:00 +0200

    multiselect working

--- resources/views/input/multiselect.blade.php
+++ resources/views/input/multiselect.blade.php
@@ -1,6 +1,4 @@
-{{-- TODO: implement multi-select using a html select to select multiple options --}}
-@extends($bsb_pkg_ref.'::form.group')
-
-@section($bsb_pkg_ref.'control_or_group')
-	@include($bsb_pkg_ref.'::form.control.select')
-@overwrite
+<?php
+$input_attributes['multiple'] = true;
+?>
+@include($bsb_pkg_ref.'::input.select')
