5f73297d4ac109e42170b7a9918919764358dc9a
bjuppa <nilsved@gmail.com>
2015-08-04 13:55:57 +0200

    stubs for multicheckbox and radiobuttons pulls in multiselect and select for now

--- resources/views/input/multicheckbox.blade.php
+++ resources/views/input/multicheckbox.blade.php
@@ -1,6 +1,5 @@
 {{-- TODO: implement multi-select using checkboxes to select multiple options - make it similar to multiselect --}}
-@extends($bsb_pkg_ref.'::form.group')
-
-@section($bsb_pkg_ref.'control_or_group')
-	@include($bsb_pkg_ref.'::form.control.select')
-@overwrite
+<?php
+$input_attributes['multiple'] = true;
+?>
+@include($bsb_pkg_ref.'::input.select')
--- resources/views/input/radiobuttons.blade.php
+++ resources/views/input/radiobuttons.blade.php
@@ -1,6 +1,2 @@
 {{-- TODO: show radiobuttons to select one option - implement this similar to select --}}
-@extends($bsb_pkg_ref.'::form.group')
-
-@section($bsb_pkg_ref.'control_or_group')
-	@include($bsb_pkg_ref.'::form.control.select')
-@overwrite
+@include($bsb_pkg_ref.'::input.select')
