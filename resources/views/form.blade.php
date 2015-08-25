e8e7efa59779bb2822434a547f1b043d30a3bbc1
bjuppa <nilsved@gmail.com>
2015-08-25 14:14:43 +0200

    added todo

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,18 +1,19 @@
 <form role="{{ $role or 'form' }}" method="{{ $method or 'POST' }}"
-@unless(empty($action))
-      action="{{ $action }}"
-        @endunless
-@unless(empty($form_id))
-      id="{{ $form_id }}"
-        @endunless
-        >
+    @unless(empty($action))
+        action="{{ $action }}"
+    @endunless
+    @unless(empty($form_id))
+        id="{{ $form_id }}"
+    @endunless
+>
     @include($bsb_pkg_ref.'::input.csrf')
     @unless(empty($form_error_content))
-    <div class="form-group has-error">
-        {!! $form_error_content !!}
-    </div>
+        <div class="form-group has-error">
+            {!! $form_error_content !!}
+        </div>
     @endunless
     @foreach($groups as $group_html)
         {!! $group_html !!}
     @endforeach
+    {{-- TODO: add an optional section of form content --}}
 </form>
