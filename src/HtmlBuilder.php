ad13e29158b723f40ab15cfdf9fbe32da9f83381
bjuppa <nilsved@gmail.com>
2015-08-25 14:15:07 +0200

    split out subfunctions

--- src/HtmlBuilder.php
+++ src/HtmlBuilder.php
@@ -4,13 +4,36 @@
 class HtmlBuilder
 {
 
-    public static $void_elements = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'menuitem', 'meta', 'param', 'source', 'track', 'wbr'];
+    public static $void_elements = [
+        'area',
+        'base',
+        'br',
+        'col',
+        'embed',
+        'hr',
+        'img',
+        'input',
+        'keygen',
+        'link',
+        'menuitem',
+        'meta',
+        'param',
+        'source',
+        'track',
+        'wbr'
+    ];
 
-    public static function element($tag_name, $attributes = [], $content = null, $default_attributes = [], $forced_attributes = [])
-    {
+    public static function element(
+        $tag_name,
+        $attributes = [],
+        $content = null,
+        $default_attributes = [],
+        $forced_attributes = []
+    ) {
         $html_string = "<$tag_name";
         $html_string .= self::compile_attributes($attributes, $default_attributes, $forced_attributes);
         $html_string .= (empty($content) and in_array($tag_name, self::$void_elements)) ? '>' : ">$content</$tag_name>";
+
         return $html_string;
     }
 
@@ -23,35 +46,9 @@ class HtmlBuilder
      */
     protected static function compile_attributes($attributes = [], $default_attributes = [], $forced_attributes = [])
     {
-        //Start with the defaults
-        $combined_attributes = self::clean_numeric_attribute_keys($default_attributes);
-        //Merge in the rest
-        foreach (self::clean_numeric_attribute_keys($attributes) as $key => $value) {
-            if ($key == 'class' and isset($combined_attributes[$key])) {
-                //Attribute class is merged from arrays of classes, defaults are not overwritten
-                $combined_attributes[$key] = array_merge((array)$combined_attributes[$key], (array)$value);
-            } else {
-                //All other defaults are overwritten
-                $combined_attributes[$key] = $value;
-            }
-        }
-        //Overwrite with any forced attributes
-        $combined_attributes = self::clean_numeric_attribute_keys($forced_attributes) + $combined_attributes;
+        $combined_attributes = self::combine_attributes($attributes, $default_attributes, $forced_attributes);
 
-        $attribute_strings = [];
-        foreach ($combined_attributes as $key => $value) {
-            if ($value !== false and !is_null($value)) {
-                if ($value === true) {
-                    $value = $key;
-                }
-                if (is_array($value)) {
-                    $value = implode($key == 'class' ? ' ' : ',', $value);
-                }
-                $attribute_strings[] = $key . '="' . e($value) . '"';
-            }
-        }
-
-        return empty($attribute_strings) ? '' : ' ' . implode(' ', $attribute_strings);
+        return self::generate_attributes_string($combined_attributes);
     }
 
     /**
@@ -73,6 +70,57 @@ class HtmlBuilder
                 $result[$key] = $value;
             }
         }
+
         return $result;
+    }
+
+    /**
+     * Combine attribute arrays into one array
+     * @param array $attributes Overwrites defaults
+     * @param array $default_attributes Defaults
+     * @param array $forced_attributes Overwrites all
+     * @return array
+     */
+    protected static function combine_attributes($attributes = [], $default_attributes = [], $forced_attributes = [])
+    {
+        //Start with the defaults
+        $combined_attributes = self::clean_numeric_attribute_keys($default_attributes);
+        //Merge in the rest
+        foreach (self::clean_numeric_attribute_keys($attributes) as $key => $value) {
+            if ($key == 'class' and isset($combined_attributes[$key])) {
+                //The attribute class is merged from arrays of classes, defaults are not overwritten
+                $combined_attributes[$key] = array_merge((array)$combined_attributes[$key], (array)$value);
+            } else {
+                //All other defaults are overwritten
+                $combined_attributes[$key] = $value;
+            }
+        }
+        //Overwrite with any forced attributes
+        $combined_attributes = self::clean_numeric_attribute_keys($forced_attributes) + $combined_attributes;
+
+        return $combined_attributes;
+    }
+
+    /**
+     * Make string of attributes for use in html tag
+     * @param array $attributes
+     * @return string
+     */
+    protected static function generate_attributes_string($attributes = [])
+    {
+        $attribute_strings = [];
+        foreach ($attributes as $key => $value) {
+            if ($value !== false and !is_null($value)) {
+                if (is_array($value)) {
+                    $value = implode($key == 'class' ? ' ' : ',', $value);
+                }
+                if ($value === true) {
+                    $value = $key;
+                }
+                $attribute_strings[] = $key . '="' . e($value) . '"';
+            }
+        }
+
+        return empty($attribute_strings) ? '' : ' ' . implode(' ', $attribute_strings);
     }
 }
